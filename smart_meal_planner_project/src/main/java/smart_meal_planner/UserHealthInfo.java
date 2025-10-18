8a53a0e602e4da6c29013c638832c89a0d6144df
abbyhxll <abbyhall016@gmail.com>
2025-10-18 17:58:59 -0500

    Add database table compatability

--- smart_meal_planner_project/src/main/java/smart_meal_planner/UserHealthInfo.java
+++ smart_meal_planner_project/src/main/java/smart_meal_planner/UserHealthInfo.java
@@ -1,28 +1,38 @@
-import java.util.ArrayList;
+package smart_meal_planner;
+import jakarta.persistence.*;
 import java.util.List;
+import java.util.ArrayList;
 
-class UserHealthInfo extends User{
-    private int userId;
+
+@Entity
+@Table(name = "User_Health_Info") class UserHealthInfo extends User {
+    @Id
+    private long uid;
+    
+    @Column(nullable=false)
     private int heightFt;
+    
+    @Column(nullable=false)
     private int heightIn;
+    
+    @Column(nullable=false)
     private int weight;
+
+    @Column(nullable=false, length=10)
     private String sex;
+
+    @Column(nullable=false, length=50)
     private String weeklyActivityLevel;
+
+    @Column(legnth=100)
     private List<String> allergies;
     
     // Constructor to initialize all fields
-    public UserHealthInfo() {
-        this.heightFt = 0;
-        this.heightIn = 0;
-        this.weight = 0;
-        this.sex = "";
-        this.weeklyActivityLevel = "";
-        this.allergies = new ArrayList<>();
-    }
+    public UserHealthInfo() {}
 
     // Getter to return the values of each field
     public int getUserId() {
-        return userId;
+        return uid;
     }
     public int getHeightFt() {
         return heightFt;
