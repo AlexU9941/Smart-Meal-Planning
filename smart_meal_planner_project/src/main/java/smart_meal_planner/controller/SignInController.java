a9cae8ab805c389a3343316c3ae10c4ddea0bd64
apreppen <apreppen@gmail.com>
2025-10-28 15:37:08 -0500

    Adding backend for password recovery
    Generates random password and sends email to specified address. Sets user's password to that random address.

--- smart_meal_planner_project/src/main/java/smart_meal_planner/controller/SignInController.java
+++ smart_meal_planner_project/src/main/java/smart_meal_planner/controller/SignInController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
+import java.security.SecureRandom;
 import java.util.Map;
 
 import jakarta.servlet.http.HttpServletRequest;
 import smart_meal_planner.model.User;
 import smart_meal_planner.service.DatabaseCommunicator;
+import smart_meal_planner.service.PasswordUtils;
+import smart_meal_planner.service.MailService;
+
+
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +25,9 @@ public class SignInController
 
     @Autowired
     private DatabaseCommunicator dbCommunicator;
+
+    @Autowired
+    private MailService mailService;
 
     @PostMapping("/sign-in")
     public ResponseEntity<?> signIn(HttpServletRequest request, @RequestBody Map<String, String> credentials) {
@@ -48,5 +56,54 @@ public class SignInController
         request.getSession().invalidate();
         return ResponseEntity.ok("Logged out");
     }
+
+
+    @PostMapping("/recover-password")
+    public ResponseEntity<?> recoverPassword(HttpServletRequest request, @RequestBody String email)
+    {
+        User user = dbCommunicator.getUserByEmail(email);
+
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found");
+            }
+
+            try{
+               
+               SecureRandom random = new SecureRandom();
+               String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+               StringBuilder tempPassword = new StringBuilder();
+
+               //random password 
+               for (int i = 0; i < 10; i++) {
+                tempPassword.append(chars.charAt(random.nextInt(chars.length())));               
+                }
+
+               //new salt 
+               String newSalt = PasswordUtils.generateSalt();
+               String hashedTempPassword = PasswordUtils.hashPassword(tempPassword.toString(), newSalt);
+               user.setPassword(hashedTempPassword);
+               user.setSalt(newSalt);
+               dbCommunicator.updateUser(user);
+
+               //recovery email
+               mailService.sendEmail(
+                   user.getEmail(), 
+                   "Password Recovery - Smart Meal Planner", 
+                   "Your temporary password is: " + tempPassword + "\nPlease log in and change your password immediately."
+               ); 
+
+                return ResponseEntity.ok("Temp password sent.");
+
+            }
+
+            catch (Exception e)
+            {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error sending password recovery email.");
+            }
+    }
+    
+
+    
 
 }
