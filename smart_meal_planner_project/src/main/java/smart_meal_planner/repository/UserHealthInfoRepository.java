62fe9fb4b7a202149976af4c54bb8180915337ca
abbyhxll <abbyhall016@gmail.com>
2025-10-31 14:02:07 -0500

    Update/Upload User Health

--- smart_meal_planner_project/src/main/java/smart_meal_planner/repository/UserHealthInfoRepository.java
+++ smart_meal_planner_project/src/main/java/smart_meal_planner/repository/UserHealthInfoRepository.java
@@ -1,7 +1,9 @@
 package smart_meal_planner.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
+
 import smart_meal_planner.model.UserHealthInfo;
 
 @Repository
